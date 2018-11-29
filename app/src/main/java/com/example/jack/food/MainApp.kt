b95a91d2f92d2049ac20839f0b7ab074547b91ff
pkitt <emosewasiefil.fodnik.4@gmail.com>
2018-11-29 20:50:17 +0700

    Add accelerometer

--- app/src/main/java/com/example/jack/food/MainApp.kt
+++ app/src/main/java/com/example/jack/food/MainApp.kt
@@ -2,6 +2,10 @@ package com.example.jack.food
 
 import android.content.Context
 import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.Keep
@@ -22,13 +26,17 @@ import kotlinx.android.synthetic.main.activity_add_food.view.*
 import kotlinx.android.synthetic.main.activity_main_app.*
 
 
-class MainApp : AppCompatActivity() {
+class MainApp : AppCompatActivity(), SensorEventListener {
 
+    private var mSensorManager : SensorManager ?= null
+    private var lastUpdate : Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_app)
 
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        lastUpdate = System.currentTimeMillis()
 
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("Data/Food")
@@ -57,4 +65,42 @@ class MainApp : AppCompatActivity() {
 
     }
 
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event!!.sensor.type == Sensor.TYPE_ACCELEROMETER){
+            getAccelerometer(event)
+        }
+    }
+
+    private fun getAccelerometer(event: SensorEvent){
+        val values = event.values
+
+        val x = values[0]
+        val y = values[1]
+        val z = values[2]
+
+        val accel = ((x*x) + (y*y) + (z*z))/(SensorManager.GRAVITY_EARTH*SensorManager.GRAVITY_EARTH)
+
+        val actualTime = System.currentTimeMillis()
+
+        if(accel >= 2){
+            if((actualTime - lastUpdate) < 200){
+                return
+            }
+
+            imageView1.setImageResource(R.drawable.start)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mSensorManager!!.registerListener(this, mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSensorManager!!.unregisterListener(this)
+    }
 }
